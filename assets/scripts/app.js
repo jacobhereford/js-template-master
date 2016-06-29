43d0ad26370dbc6cf6cad79e23f298e84b63ef0e
jacobhereford <jacobhereford@gmail.com>
2016-06-29 09:39:42 -0400

    ddd

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,10 @@
 'use strict';
 
 module.exports = {
-  host: 'http://agile-hollows-29875.herokuapp.com',
+  host: 'http://localhost:3000',
 };
+
+//http://agile-hollows-29875.herokuapp.com
 
 ///// same thing
 ///http://grunt-todo.herokuapp.com/
